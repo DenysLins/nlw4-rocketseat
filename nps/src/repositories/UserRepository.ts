a1b7b1b6d34e698a5175eb9bf1aefe5b07d94909
Denys Lins <denys.lins@hp.com>
2021-03-03 16:27:48 -0300

    Refact user controller

---
+++ nps/src/repositories/UserRepository.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository } from "typeorm";
+import { User } from "../models/User";
+
+@EntityRepository(User)
+class UserRepository extends Repository<User> {
+
+
+
+
+  
+}
+
+export { UserRepository };
