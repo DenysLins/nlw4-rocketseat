300b51f586e3125eb922dbe0bf837da8f6e619a4
Denys Lins <denys.lins@hp.com>
2021-03-04 16:31:34 -0300

    Finish surveys get

--- nps/src/controllers/SurveysController.ts
+++ nps/src/controllers/SurveysController.ts
@@ -6,6 +6,15 @@ class SurveysController {
   async create(request: Request, response: Response) {
     const { title, description } = request.body;
     const surveysRepository = getCustomRepository(SurveysRepository);
+
+    const survey = surveysRepository.create({
+      title,
+      description,
+    });
+
+    await surveysRepository.save(survey);
+
+    return response.status(201).json(survey);
   }
 }
 
--- nps/src/router.ts
+++ nps/src/router.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
+import { SurveysController } from "./controllers/SurveysController";
 import { UsersController } from "./controllers/UsersController";
 
 const router = Router();
 
 const usersController = new UsersController();
+const surveysController = new SurveysController();
 
 const prefix = "/api";
 
 router.post(`${prefix}/users`, usersController.create);
+router.post(`${prefix}/surveys`, surveysController.create);
 
 export { router };
